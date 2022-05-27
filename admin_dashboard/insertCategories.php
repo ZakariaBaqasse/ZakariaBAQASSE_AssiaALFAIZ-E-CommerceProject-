75bdf08ec35b90d20c576668988dab9a0ac3abf6
zakariaBAQASSE <zbaqasse51@gmail.com>
2022-05-27 22:35:19 +0100

    insert categorie in categoryByGender

--- admin_dashboard/insertCategories.php
+++ admin_dashboard/insertCategories.php
@@ -1,17 +1,18 @@
 <?php
  include_once('../includes/connectDatabase.php');
  if(isset($_POST['insert-categ'])){
-    if(!empty($_POST['categ-title'])){
+    if(!empty($_POST['categ-title']) ){
       $categorie_title = htmlspecialchars($_POST['categ-title']);
+      $gender_id = $_POST['categorie_gender'];}
       try{
-      $selectSmt = $db->prepare("SELECT * FROM `categories` WHERE categorie_title = '$categorie_title'");
+      $selectSmt = $db->prepare("SELECT * FROM `categories` WHERE categorie_title = '$categorie_title' and gender_id=$gender_id");
       $selectSmt->execute();
       $title = $selectSmt->fetchColumn();
       if($title>0){
         echo "Categorie already exists in database";
       }
       else{
-      $stmt = $db->prepare("INSERT INTO `categories` (categorie_title) values ('$categorie_title')");
+      $stmt = $db->prepare("INSERT INTO `categories` (categorie_title,gender_id) values ('$categorie_title',$gender_id)");
       if($stmt->execute()){
         echo '<p class="successInsert">Category has been inserted successfully</p>';
       }
@@ -23,7 +24,7 @@
     else{
       echo "Title cannot be empty";
     }
- }
+ 
 ?>
 <h2 class="text-center mt-1">Insert Category</h2>
 <form action="" method="POST" class="mb-3 mt-5">
@@ -31,6 +32,22 @@
   <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
   <input type="text" class="form-control" placeholder="Insert Categorie name" aria-label="Categories" aria-describedby="basic-addon1" name="categ-title">
 </div>
+<div class="form-outline mb-2 w-90 m-auto">
+                 <select name="categorie_gender" id="" class="form-select">
+                     <option value="">Select a Gender</option>
+                     <?php
+                        $SelectStmt = $db->prepare('SELECT * FROM `gender`');
+                        $SelectStmt->execute();
+                        $genders = $SelectStmt->fetchAll();
+                        foreach($genders as $gender){
+                           $title = $gender['gender'];
+                            $id = $gender['gender_id'];
+                            echo "<option value='$id'>$title</option>";
+                
+                        }
+                     ?>
+                 </select>
+             </div>
 <div class="input-group w-10 mb-2">
   <input type="submit" class="bg-info border-0 p-2 my-3" value = "Insert Categorie"aria-label="Username" aria-describedby="basic-addon1" name="insert-categ">
   
